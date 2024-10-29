8ec7b431401061037632855238dbe5e8cae908df
Martin Kierkegaard <MARK@zealand.dk>
2024-10-29 08:39:57 +0100

    find fem fejl

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const app = vue.createApp({
+    data() {
+        return {
+            iintro: 'Welcome to my Vue template',
+        }
+    },
+    methods: {
+        myMethod(){
+
+        },
+    },
+    computed: {
+        myComputed() {
+            return ''
+        },
+        
+    }
+})
